92bb1c4e8191e337e28b836904c36da5506df563
0xDEAFBABE <deafbabe@startmail.com>
2022-09-01 00:07:30 +0600

    Write tests

--- tests/math.test.ts
+++ tests/math.test.ts
@@ -1,7 +1,11 @@
-import { asserts } from "./deps.ts";
+import { assertEquals } from "https://deno.land/std@0.153.0/testing/asserts.ts";
 
-import { add } from "../src/math.ts";
+import { add, subtract } from "../src/math.ts";
 
 Deno.test(function addTest() {
-  asserts.assertEquals(add(2, 3), 5);
+  assertEquals(add(2, 3), 5);
+});
+
+Deno.test(function subtractTest() {
+  assertEquals(subtract(5, 3), 2);
 });
